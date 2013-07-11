ac6fda0e45bdace2bc5498bf51b66414d5627cac
stuzl <christopherhowitt@hotmail.com>
2013-07-11 11:51:00 +0100

    fix grader.js

--- grader.js
+++ grader.js
@@ -22,8 +22,7 @@ function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // 
-http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
