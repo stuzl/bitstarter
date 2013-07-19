47c055248d57872ff75149baaf9936ca3796456e
stuzl <christopher@hotmail.com>
2013-07-19 23:33:04 +0000

    move images to folder, add image folder ref to web.js

--- web.js
+++ web.js
@@ -6,7 +6,10 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('./index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/images'));
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
